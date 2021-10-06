2437926d607753c25741a4a2718fa8a888295820
William Wong <compulim@users.noreply.github.com>
2021-10-06 14:35:09 -0700

    Add samples

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
         should narrate the link "a Wikipedia article, link" (or something similar). It should NOT narrate "a Wikipedia
         article, message".
       </p>
+      <p>Windows Narrator with CAPS + up/down arrow: "Bullet, this article is based on, link, a Wikipedia article."</p>
+      <p>VoiceOver (macOS) with up/down arrow: "Bullet, this article is based on, link, a Wikipedia article."</p>
+      <p>VoiceOver (iPadOS) with two-finger swipe: "This article based on a Wikpiedia article, link."</p>
+      <h1>Try these out</h1>
+      <p><small>bullet</small> this article is based on <small>link</small> a wikipedia article</p>
+      <p><ins>bullet</ins> this article is based on <ins>link</ins> a wikipedia article</p>
+      <h1>Sample</h1>
       <ul aria-roledescription="transcript">
         <li>
           <div aria-roledescription="activity">
